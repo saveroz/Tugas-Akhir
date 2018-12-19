98bcd2fdd8db807e14ca40391fbdb8d7c98bb1d9
Muhammad Savero <36983520+saveroz@users.noreply.github.com>
2018-12-19 19:02:23 +0700

    Update InaTEWS.py

--- RaspiAsSender/InaTEWS.py
+++ RaspiAsSender/InaTEWS.py
@@ -16,41 +16,46 @@ ke thingspeak
 """
 class Example (rockBlockProtocol):
     
-    SLEEP_INTERVAL = 1
+        SLEEP_INTERVAL = 0.5 # delay interval pengiriman 
            
     def main(self):
-        port="/dev/ttyACM0"
+        port="/dev/ttyACM0" # inialiasasi port arduino
     
         while(True):
             try:
                 ser=serial.Serial(port,9600)
                 data=ser.readline()
-                data_split=data.split(",")
-                val1=data_split[0]
+                data_split=data.split(",") # data dibagi menjadi 3 bagian 
+                val1=data_split[0] 
                 val2=data_split[1]
-                val3=data_split[2]   
-                self.emit(val1,val2,val3)
-            except serial.serialutil.SerialException:
+                val3=data_split[2]
+            except serial.serialutil.SerialException: #deteksi error Serial
                 print "port gak kedeteksi broo!!, coba ane ganti portnya dulu"
-                if port=="/dev/ttyACM1":
+                if port=="/dev/ttyACM1": #ganti port ketika terjadi error
                     port="/dev/ttyACM0"
                 elif port=="/dev/ttyACM0":
                     port="/dev/ttyACM1"
-                
             except:
                 print "unknown error tapi program tetap harus berjalan"
+            else:
+                try:
+                    if val1!=None and val2!=None and val3!=None:
+                        self.emit(val1,val2,val3)
+                except: #ketika terjadi error
+                    print "pengiriman error"
+                else:
+                    time.sleep(10) #delay ketika pengiriman berhasil
             finally:    
-                time.sleep( self.SLEEP_INTERVAL)
+                time.sleep(self.SLEEP_INTERVAL)
             
                                 
     def emit(self, val1,val2,val3):
             
         rb = rockBlock.rockBlock("/dev/ttyUSB0", self)
         
-        rb.sendMessage(","+ str(val1)+","+str(val2)+","+str(val3))
+        rb.sendMessage(","+ str(val1)+","+str(val2)+","+str(val3)+","+"0"+","+"0"+","+"0")
                                                                                   
         rb.close()
-         
     def rockBlockTxStarted(self):
         logA=datetime.datetime.now()
         print "rockBlockTxStarted"
